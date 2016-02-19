efc34debd481480d726ec976fbc5c7e6cad6fc21
AndreyShevchenko <andrey_nk@ukr.net>
2016-02-19 16:12:58 +0200

    delete file

--- javaPractice/src/MainClass.java
+++ javaPractice/src/MainClass.java
@@ -1,7 +1,4 @@
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
-import java.util.TreeSet;
 
 /**
  * Created by Андрей Шевченко on 06.02.2016.
@@ -9,7 +6,7 @@ import java.util.TreeSet;
 public class MainClass {
     public static void main(String[] args) {
         int size = 2;
-        Module36 a = new Module36(size);
+        BinaryHeap a = new BinaryHeap(size);
         Random random = new Random();
         int[] x = {0, 1, 2};
         for (int i = 0; i < x.length; i++) {
